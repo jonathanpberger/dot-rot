a73006aa0c06c123fc86ca0627d83d9c2b8d9dd3
JPB <jonathan@ltmp.co>
2023-02-06 10:40:32 -0500

    Extract list of `brew`’d apps to `brew.sh`.

---
+++ brew.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+brew visual-studio-code
+brew install iterm2
+brew install zoom
+brew install firefox
+brew install phoenix
+brew install slack
+brew install flycut
+brew install rowanj-gitx
+brew install font-sf
+brew install font-san-francisco
+brew install font-sanfrancisco
+brew install font-noto
+brew install font-noto-sans
+brew install font-noto-serif
+brew install font-noto-sans-display
+brew install font-fira-mono-for-powerline
+brew install font-inconsolata-for-powerline
+brew install ack
+brew install skitch
+brew install tree
+brew install docker-desktop
+brew install pg
+brew install postgres
+brew install postgresql
+brew install cocoapods
+brew install imageoptim
+brew install keycastr
+brew install pd
+brew install chromeless
+brew install whatsapp
+brew install emojify
+brew install signal
+brew install cowsay
+brew install telegram
+brew install diffusionbee
+brew install openpomodoro-cli
