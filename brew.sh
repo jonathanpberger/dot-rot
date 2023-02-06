f83a98a978cfbcb4d776458e67869442bab884ac
JPB <jonathan@ltmp.co>
2023-02-06 12:06:08 -0500

    Fix some broken brew commands.

--- brew.sh
+++ brew.sh
@@ -1,38 +1,43 @@
 #!/bin/sh
 
-brew visual-studio-code
-brew install iterm2
-brew install zoom
+brew install --cask telegram
+brew install ack
+brew install cocoapods
+brew install cowsay
+brew install emojify
 brew install firefox
-brew install phoenix
-brew install slack
 brew install flycut
-brew install rowanj-gitx
-brew install font-sf
-brew install font-san-francisco
-brew install font-sanfrancisco
 brew install font-noto
 brew install font-noto-sans
 brew install font-noto-serif
-brew install font-noto-sans-display
+brew install font-san-francisco
+brew install font-sanfrancisco
+brew install font-sf
+brew install imageoptim
+brew install iterm2
+brew install keycastr
+brew install pd
+brew install phoenix
+brew install powerline-go
+brew install rowanj-gitx
+brew install signal
+brew install skitch
+brew install slack
+brew install tree
+brew install whatsapp
+brew install zoom
+brew install --cask visual-studio-code
+
+# go get -u github.com/open-pomodoro/openpomodoro-cli/cmd/pomodoro
+
+# Error'd out
+brew tap homebrew/cask-fonts         # You only need to do this once!
 brew install font-fira-mono-for-powerline
 brew install font-inconsolata-for-powerline
-brew install ack
-brew install skitch
-brew install tree
+brew install font-noto-sans-display
 brew install docker-desktop
 brew install pg
 brew install postgres
 brew install postgresql
-brew install cocoapods
-brew install imageoptim
-brew install keycastr
-brew install pd
 brew install chromeless
-brew install whatsapp
-brew install emojify
-brew install signal
-brew install cowsay
-brew install telegram
 brew install diffusionbee
-brew install openpomodoro-cli
